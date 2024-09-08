401124fab7c3eb29019dbae66c02c98fce0be082
JPechberty <j.pechberty@gmail.com>
2024-09-08 20:00:18 +0200

    Start project

--- README.md
+++ README.md
@@ -3,3 +3,16 @@
 [![Version](https://img.shields.io/badge/Version-1.0.0-blue)]()
 
 ## Objectifs
+
+Importer des fichiers CSV et de les manipuler en utilisant des HashMaps.
+
+Vous devrez réaliser une application permettant de fournir les résultats d'une élection 
+à partir de liste des bulletins de votes fournis en CSV. ( Vous disposez de 2 fichiers csv dans le dossier `/docs`)
+
+2 types de résultats seront à fournir :
+- La ventilation des votes au niveau national
+- La ventilation des votes pour chaque bureau de votes.
+
+## Screenshots interfaces
+
+![screen](./docs/interface.png)
--- src/main/java/com/courses/javatd2sio2/HelloController.java
+++ src/main/java/com/courses/javatd2sio2/HelloController.java
@@ -1,14 +1,50 @@
 package com.courses.javatd2sio2;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
 
-public class HelloController {
-    @FXML
-    private Label welcomeText;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HelloController implements Initializable {
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private ComboBox cbCities;
+    @FXML
+    private TableColumn tcCandidat;
+    @FXML
+    private TableColumn tcNbVoix;
+    @FXML
+    private TableView tvResults;
+    @FXML
+    private Button btnImportData;
+    @FXML
+    private Label lblSelectedFile;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        lblSelectedFile.setText("Select a file to import data");
+    }
+
+    @FXML
+    protected void onSelectFileBtnClick() throws IOException {
+
+    }
+
+    @FXML
+    protected void onImportBtnClick(){
+
+    }
+
+    @FXML
+    protected void onNationalResultsBtnClicked(){
+
+    }
+
+    @FXML
+    protected void onPollingStationResultsBtnClicked(){
+
     }
 }
